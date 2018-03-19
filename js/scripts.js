4aade5d9c25fcd37db74b2a8cb179f0268a4a8eb
andrew lupton and eric wolfe <eric@>
2018-03-19 16:51:18 -0700

    add reverse function

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,15 @@
 
   var sentence = prompt("Write a sentence")
-  alert(sentence);
+  console.log(sentence);
   var char1=sentence.charAt(0);
-  alert(char1);
   var char2=sentence.charAt(sentence.length-1);
-
-  alert(char2);
-  alert(char1 + char2);
 
   var newstring = (char1 + char2);
   var newnewstring = newstring.toUpperCase();
 
-  alert(newnewstring);
-
   // var str = newnewstring.reverse(){
-    function reverse(newnewstring){
-      return newnewstring.split("").reverse().join("");
+  function reverse(inputString){
+      return inputString.split("").reverse().join("");
   }
 
-  alert(reverse);
+  alert(reverse(newnewstring));
